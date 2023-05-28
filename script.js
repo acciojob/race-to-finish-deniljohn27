02127ea10fbc7a70ca29e25e9a0fa0ad1873acee
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-28 22:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let di=document.getElementById("output");
+for(let i=1;i<=5;i++){
+	promises.push(new Promise((resolve)=>{setTimeout(()=>{resolve(i)},i)}))
+}
+let pro=Promise.any(promises);
+pro.then((cont)=>{di.innerText=cont});
